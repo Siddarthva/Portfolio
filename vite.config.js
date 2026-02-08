b062e9df85513c3d2f87995ac16ee86d6857b022
Siddarthva <sidarthvacharyaa@gmail.com>
2026-02-08 10:41:40 +0530

    Configure vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  base:"/Portfolio/",
-  resolve:{
-    alias:{
-      "@":path.resolve(__dirname,"./src")
+  plugins: [react(), tailwindcss()],
+  base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
     }
   }
 })
